ea49e0c822fb365744f86552a08cace7999c6aa9
ZHM89 <z.maneshi@gmail.com>
2024-12-02 14:05:00 +0100

    none here!

--- code.py
+++ code.py
@@ -7,3 +7,8 @@ x = np.linspace(0, 1, 100)
 y = np.sin(x)
 plt.plot(x, y)
 # added some lines here to modify the code
+# some more lines will be added here
+
+x1 = np.linspace(0, 10, 100)
+y1 = np.cos(x1)
+plt.plot(x1, y1)
